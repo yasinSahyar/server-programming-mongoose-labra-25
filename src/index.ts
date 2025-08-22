93ce56acbd644be7b214a805dce8a9c418a4b210
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2025-08-22 10:31:57 +0300

    feat: integrate MongoDB connection on server start
    - Added mongoConnect function to establish a connection to MongoDB before starting the server.
    - Updated index.ts to listen on the specified port only after a successful database connection.
    - Enhanced error handling in db.ts to log meaningful messages if the connection fails or if the DB_URL is not set.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import app from './app';
+import mongoConnect from './utils/db';
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Listening: http://localhost:${port}`);
-});
+
+mongoConnect()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Listening: http://localhost:${port}`);
+    });
+  })
+  .catch(() => {
+    console.log('Server not started!');
+  });
--- src/utils/db.ts
+++ src/utils/db.ts
@@ -3,7 +3,16 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const mongoConnect = async () => {
-  // Connect to MongoDB
+  try {
+    if (!process.env.DB_URL) {
+      throw new Error('Database url not found in .env file');
+    }
+    const connection = await mongoose.connect(process.env.DB_URL);
+    console.log('DB connected successfully');
+    return connection;
+  } catch (error) {
+    console.error('Connection to db failed: ', (error as Error).message);
+  }
 };
 
 export default mongoConnect;
